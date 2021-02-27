39664126652cc083918bdf69c2e66cf63e728779
c0ffein <c0ffein@users.noreply.github>
2021-02-27 11:54:43 +0100

    [ADD] init hof map

---
+++ tiles/firebarrel.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="firebarrel" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="../src/firebarrel.png" width="128" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
